90e80456bf247c8a8f0c51c596a50c8ccd5401bf
carolina barbosa <carolinamsanto@gmail.com>
2024-06-16 22:36:28 -0300

    projeto edição foto

--- projetobaseexpo-main/app/index.tsx
+++ projetobaseexpo-main/app/index.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  View} from 'react-native';
+
+import Button from '../components/Button';
+import ImageViewer from '../components/ImageViewer';
+
+const imagem = require('../assets/images/background-image.png');
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+         <ImageViewer placeholderImageSource={imagem}/>
+       </View>
+       <View style={styles.footerContainer}>
+        <Button theme='primary' label="choose a photo" />
+        <Button theme='primary' label="use this photo" />
+       </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +28,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer:{
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
