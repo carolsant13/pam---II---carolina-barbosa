88b33b62c2dca4a7be67f5c56ce930e0fb95a38c
carolina barbosa <carolinamsanto@gmail.com>
2024-06-24 23:34:30 -0300

    projeto de editor de foto

--- projetobaseexpo-main/app/index.tsx
+++ projetobaseexpo-main/app/index.tsx
@@ -1,22 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,  View} from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
 
-import Button from '../components/Button';
-import ImageViewer from '../components/ImageViewer';
+ import Button from '../components/button';
+ import ImageViewer from '../components/ImageViewer'; 
+ import * as ImagePicker from 'expo-image-picker';
 
-const imagem = require('../assets/images/background-image.png');
-
+const PlaceholderImage = require('../assets/images/background-image.png');
 
 export default function App() {
+    const [selectedImage, setSelectedImage] = useState(null);
+    
+    const pickImageAsync = async () =>{
+        let result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!result.canceled){
+           setSelectedImage(result.assets[0].uri);
+        } else{
+            alert('you did not select any image');
+        }
+    }
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-         <ImageViewer placeholderImageSource={imagem}/>
-       </View>
-       <View style={styles.footerContainer}>
-        <Button theme='primary' label="choose a photo" />
-        <Button theme='primary' label="use this photo" />
-       </View>
+        <ImageViewer 
+        placeholderImageSource={PlaceholderImage}
+        selectedImage={selectedImage}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="choose a photo" onPress={pickImageAsync} />
+        <Button theme="any" label="use this photo" onPress={"any"}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -27,14 +45,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  imageContainer:{
+  imageContainer: {
     flex: 1,
     paddingTop: 58,
   },
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
-  },
+  }
 });
+
--- projetobaseexpo-main/components/ImageViewer.jsx
+++ projetobaseexpo-main/components/ImageViewer.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Image } from 'react-native';
 
-export default function ImageViewer({ placeholderImageSource }) {
+export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+  const imageSource = selectedImage ? { uri: selectedImage } : placeholderImageSource;
   return (
     <Image source={placeholderImageSource} style={styles.image} />
   );
